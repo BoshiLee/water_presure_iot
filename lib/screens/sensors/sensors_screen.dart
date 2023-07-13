6e9e8d37d93dbaaee8f355cf837854d56bbd9373
Boshi <boshi.litw@gmail.com>
2023-07-13 17:30:29 +0800

    更新樣式

--- lib/screens/sensors/sensors_screen.dart
+++ lib/screens/sensors/sensors_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sidebarx/sidebarx.dart';
 import 'package:water_pressure_iot/cubits/sensors_cubit.dart';
 import 'package:water_pressure_iot/screens/sensors/dashboard_card.dart';
+import 'package:water_pressure_iot/screens/sensors/dashboard_value_card.dart';
 import 'package:water_pressure_iot/screens/widgets/static_pagination_list_view.dart';
 
 class SensorsScreen extends StatelessWidget {
@@ -35,56 +36,8 @@ class SensorsScreen extends StatelessWidget {
                   children: [
                     Container(
                       constraints: const BoxConstraints(maxWidth: 300),
-                      child: DashBoardCard(
-                        margin: const EdgeInsets.only(
-                            left: 16, right: 8, bottom: 16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            const Text(
-                              'Latest Pressure Value:',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.black87,
-                              ),
-                            ),
-                            const Expanded(
-                              child: Center(
-                                child: Text.rich(
-                                  TextSpan(
-                                    text: '0',
-                                    style: TextStyle(
-                                      overflow: TextOverflow.ellipsis,
-                                      fontSize: 105,
-                                      fontFamily: 'Roboto',
-                                    ),
-                                    children: <TextSpan>[
-                                      TextSpan(
-                                        text: 'kgf',
-                                        style: TextStyle(
-                                          fontSize: 26,
-                                          overflow: TextOverflow.ellipsis,
-                                          fontFamily: 'Roboto',
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  strutStyle: StrutStyle(
-                                    fontFamily: 'Roboto',
-                                    height: 1.0,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Text(
-                              'Sensor $index ',
-                              style: const TextStyle(
-                                fontSize: 15,
-                                color: Colors.black87,
-                              ),
-                            ),
-                          ],
-                        ),
+                      child: DashboardValueCard(
+                        sensor: context.read<SensorsCubit>().sensors[index],
                       ),
                     ),
                     Expanded(
