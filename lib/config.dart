bed01f1b7eadf81a4ee92e7075f3a558d59b5115
Boshi <boshi.litw@gmail.com>
2023-08-08 16:58:55 +0800

    新增連線設定

--- lib/config.dart
+++ lib/config.dart
@@ -8,7 +8,7 @@ class Config {
       case Flavor.PRODUCTION:
         return 'prod.nbiot.itri.org.tw';
       case Flavor.DEVELOPMENT:
-        return '192.168.1.169:5000';
+        return 'irtik900.asuscomm.com:50';
       case Flavor.LOCALHOST:
         return '127.0.0.1:5000';
 
