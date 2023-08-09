7186154bf10d41fe3892e383e2eb0457ad85ce82
Boshi <boshi.litw@gmail.com>
2023-08-09 17:30:11 +0800

    add mark

--- lib/provider/login_provider.dart
+++ lib/provider/login_provider.dart
@@ -3,7 +3,7 @@ import 'package:water_pressure_iot/models/login_auth.dart';
 
 class LoginProvider {
   static final ApiBaseHelper _helper = ApiBaseHelper();
-  static const String _route = '/login';
+  static const String _route = '/accounts/login';
 
   Future<Map<String, dynamic>?> login({required LoginAuth auth}) async {
     return await _helper.post(
